d22dfd0ffa0804714ab8d8f6bde96478209beb0a
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-04-15 00:04:37 -0300

    Simple webserver using Express

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
+//     let salida = {
+//         nombre: 'Juan Pablo',
+//         apellido: 'Tyrlik',
+//         url: req.url
+//     };
+
+//     res.send(salida);
+// });
+
+app.listen(3000, () => {
+    console.log('Escuchando el puerto 3000');
+});
