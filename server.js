2c635ca1eb5f32759dd7d55e13af80196bfe7356
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-04-15 23:17:12 -0300

    Views created, Partials (Components) created, and HBS helpers

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
 
 app.use(express.static(__dirname + '/public'));
 
+// HBS Engine
+
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+
+// Routes
 
 app.get('/', (req, res) => {
     res.render('home', {
-        nombre: 'Juan Pablo',
-        anio: new Date().getFullYear()
+        nombre: 'juan pablo'
     });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(3000, () => {
